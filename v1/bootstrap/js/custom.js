cf551776672dd460d6810744dbde20d19e3b998f
Mazeyar Rezaei <mazeyarr@gmail.com>
2018-02-14 21:02:46 +0100

    optimized functions and fixed bugs

--- v1/bootstrap/js/custom.js
+++ v1/bootstrap/js/custom.js
@@ -1,6 +1,6 @@
 /**
  * @author Mazeyar Rezaei Ghavamabadi
- * @date 12-02-2018
+ * @date 14-02-2018
  * @description This was an assignment to examine my web and coding knowledge, by building an elevator application
  */
 $(document).ready(function (doc) {
@@ -10,10 +10,11 @@ $(document).ready(function (doc) {
      * @type {number}
      * @type {Object}
      */
-    var elevatorLevel = 1,
-        elevator = {
+    var elevator = {
+        level:  1,
         moving: false,
         speed: 5000,
+        delay: 3000,
         destanations: []
     };
     /**
@@ -35,59 +36,105 @@ $(document).ready(function (doc) {
      * @param callLevel this is the number of the floor that has been called
      */
     function handleCall(callLevel) {
-        /**
-         * if the elevator is already on the destination floor there is no need to move the elevator.
-         */
-        if(callLevel === elevatorLevel) {
-            swal('Whoops', 'The elevator is already on this floor','warning');
-            return;
-        }
+        if (callLevel !== elevator.level){
 
-        /**
-         * This simulates the doors closing of the elevator
-         * so that you can't press on the buttons inside the elevator, from outside the elevator
-         *
-         * @type {*|jQuery|HTMLElement}
-         */
-        var elavatorButtons = $('.elevatorButtons-' + callLevel);
-        elavatorButtons.removeClass('btn-info');
-        elavatorButtons.addClass('btn-success');
-
-        /**
-         * If the elevator is already moving there's no need to start the elevator again,
-         * because its already moving to another destination.
-         *
-         * if the elevator is not moving the floor that's calling the elevator will be added to the destinations array,
-         * and start moving the elevator by calling the function moveElevator(int)
-         */
-        if (elevator.moving) {
             /**
-             * If floor that's calling the elevator is already in the list of destinations to go to.
-             * it returns an error, else we add the floor to the destinations array
+             * Updating the called floors by highlighting them
+             *
+             * @type {*|jQuery|HTMLElement}
              */
+            var elavatorButtons = $('.elevatorButtons-' + callLevel);
+            elavatorButtons.removeClass('btn-info');
+            elavatorButtons.addClass('btn-success');
+
             if(elevator.destanations.indexOf(callLevel) === -1) {
                 elevator.destanations.push(callLevel);
 
                 /**
-                 * This function sorts the array so that, example: call floor: 3, call floor: 5, call floor: 4
-                 * this function make sure the elevator will first go to floor: 3 then floor: 4 and finally floor: 5
-                 * @type {Array}
+                 * If the elevator is going up the elevator will arrange the called floors in a ascending order
+                 *
+                 * If the elevator is coming from above the elevator will rearrange the array in a descending order, so that it
+                 * honours the floors that are in between.
                  */
-                elevator.destanations = elevator.destanations.sort(function (a, b) {  return a - b;  });
-            }else {
+                if(elevator.level > callLevel){
+                    elevator.destanations = elevator.destanations.sort(function (a, b) {  return b - a;  });
+                }else{
+                    elevator.destanations = elevator.destanations.sort(function (a, b) {  return a - b;  });
+                }
+
+                /**
+                 * if the elevator is not moving then we initiate it.
+                 */
+                if (!elevator.moving){
+                    elevator.moving = true;
+                    setTimeout(function () {
+                        moveElevator();
+                    }, elevator.delay);
+                }
+            }else{
                 swal('Be patient...', 'We will get there in a moment...','info');
             }
         }else{
-            elevator.destanations.push(callLevel);
+            swal('Whoops', 'The elevator is already on this floor','warning');
+        }
+    }
+
+    function moveElevator() {
+        /**
+         * Update the GUI
+         */
+        elevatorState('doors-close');
+        elevatorState('moving');
+        $('#level-' + elevator.level).removeClass('level-active');
+        $('.elevator-buttons-' + elevator.level).hide('slow');
+
+        /**
+         * Simulate the elevator moving by using a timer
+         */
+        setTimeout(function () {
+            /**
+             * Update GUI
+             */
+            elevatorState('arrived');
+            elevatorState('doors-open');
+
+            elevator.level = elevator.destanations[0]; // Set the elevator level to the destination
 
             /**
-             * This function sorts the array so that, example: call floor: 3, call floor: 5, call floor: 4
-             * this function make sure the elevator will first go to floor: 3 then floor: 4 and finally floor: 5
-             * @type {Array}
+             * Updating the called floors by highlighting them
+             *
+             * @type {*|jQuery|HTMLElement}
              */
-            elevator.destanations = elevator.destanations.sort(function (a, b) {  return a - b;  });
-            moveElevator(callLevel);
-        }
+            var elevatorButtons = $('.elevatorButtons-' + elevator.level);
+            elevatorButtons.addClass('btn-info');
+            elevatorButtons.removeClass('btn-success');
+
+            /**
+             * Update GUI
+             */
+            $('.elevator-buttons-' + elevator.level).show('slow');
+
+            /**
+             * Remove the destination floor from the array on arrival
+             */
+            elevator.destanations.splice(elevator.destanations.indexOf(elevator.level), 1);
+            $('#level-' + elevator.level).addClass('level-active');
+
+            /**
+             * If there are other destinations the elevator needs to go to this function is called again.
+             * else the elevator will put it self in an 'offline' state by calling the elevatorState methods
+             */
+            setTimeout(function () {
+                if (typeof elevator.destanations[0] !== 'undefined') {
+                    moveElevator();
+                }else {
+                    elevator.moving = false;
+                    elevator.timerRunning = null;
+                    elevatorState('offline');
+                    elevatorState('doors-close');
+                }
+            }, elevator.delay);
+        }, elevator.speed);
     }
 
     /**
@@ -133,84 +180,4 @@ $(document).ready(function (doc) {
         }
     }
 
-    /**
-     * This function will move the elevator to all destinations
-     * @param to This is a the number of the floor it needs to go.
-     */
-    function moveElevator(to) {
-
-        /**
-         * We remove the active class from the floor the elevator is at right now.
-         * @type {*|jQuery|HTMLElement}
-         */
-        var fromLevel = $('#level-' + elevatorLevel);
-        fromLevel.removeClass('level-active');
-
-        /**
-         * We simulate the elevator doors closing, using the 'elevatorState' method
-         */
-        setTimeout(function(){
-            $('.elevator-buttons-' + elevatorLevel).hide('slow');
-            elevatorState('doors-close');
-        }, 500);
-
-        /**
-         * we set the elevator property 'moving' on TRUE
-         * @type {boolean}
-         */
-        elevator.moving = true;
-
-        elevatorState('moving');
-
-        /**
-         * We simulate the elevator moving by setting a timer to the speed of the elevator
-         */
-        setTimeout(function(){
-            /**
-             * When arrived the elevator state changes
-             */
-            elevatorState('arrived');
-            elevatorState('doors-open');
-
-            /**
-             * the elevator floor buttons are now visible again on the corresponding floor.
-             * The destination floor, so the floor that the elevator moved to will be removed from the array by using the .splice methd
-             * this method will auto rearrange the array so that there are no null holes in the array.
-             */
-            $('.elevator-buttons-' + to).show('slow');
-            elevator.destanations.splice(elevator.destanations.indexOf(to), 1);
-
-            /**
-             * We also visually update the floor buttons inside the elevator by giving them a colour.
-             * @type {*|jQuery|HTMLElement}
-             */
-            var elevatorButtons = $('.elevatorButtons-' + to);
-            elevatorButtons.addClass('btn-info');
-            elevatorButtons.removeClass('btn-success');
-
-            /**
-             * We update the floor where the elevator is right now and give the floor a green colour.
-             */
-            elevatorLevel = to;
-            $('#level-' + elevatorLevel).addClass('level-active');
-
-            /**
-             * We set a new timer to simulate people leaving the elevator and/or coming in.
-             * When the timer hits 0 the function will check if there are other destinations to go to and executes this method again.
-             * else the elevator will place it self in a state of 'offline'
-             */
-            setTimeout(function(){
-                if(elevator.destanations[0] !== undefined) {
-                    elevatorState('moving');
-                    elevatorState('doors-close');
-
-                    moveElevator(elevator.destanations[0]);
-                }else{
-                    elevator.moving = false;
-                    elevatorState('offline');
-                    elevatorState('doors-close');
-                }
-            }, 5000);
-        }, elevator.speed);
-    }
 });
